2df90a0902f28f5687b82dc082a0c0244e7c9a77
Никита Кирьян <145542086+Xopuc@users.noreply.github.com>
2024-11-26 12:14:27 +0300

    Update _env.js

--- src/mainTable/_env.js
+++ src/mainTable/_env.js
@@ -1,15 +1,15 @@
-const MAIN_SS_ID = "1akE7Tk4pVugrKfLBvs0AvW8rXnY_OdaOGuhMLWU-Nh0"   // ID Главной таблицы
-const REMAINS_TECH_SS_ID = "14upu2FrMQaARlAgr-NiiHpgvLO19N7egOr0NzHvuyP0"   // ID Таблицы для выгрузки главная
+const MAIN_SS_ID = ""   // ID Главной таблицы
+const REMAINS_TECH_SS_ID = ""   // ID Таблицы для выгрузки главная
 
 const PURCHASING_SHEET_ID = "0" // ID листа "Закупки"
-const PLACEMENTS_SHEET_ID = "1838879247"    // ID листа "Размещения"
-const SHIPPING_SHEET_ID = "1408313062"  // ID листа "Отгрузки"
-const REMAINS_SHEET_ID = "562607511"    // ID листа "Остатки"
-const REMAINS_TECH_SHEET_ID = "4469970"    // ID листа "Остатки (для выгрузки)"
-const UNPLACED_REMAINS_SHEET_ID = "1173719455"  // ID листа "Остатки не распределенные"
-const SEARCH_SHEET_ID = "178869476" // ID листа "Поисковик"
-const CART_SHEET_ID = "1031805096"  // ID листа "Корзина"
-const EXCEL_REGISTRY_SHEET_ID = "1314426788"    // ID листа "Реестр файлов"
-const HANDBOOK_SHEET_ID = "1867466559"  // ID листа "Справочник"
+const PLACEMENTS_SHEET_ID = ""    // ID листа "Размещения"
+const SHIPPING_SHEET_ID = ""  // ID листа "Отгрузки"
+const REMAINS_SHEET_ID = ""    // ID листа "Остатки"
+const REMAINS_TECH_SHEET_ID = ""    // ID листа "Остатки (для выгрузки)"
+const UNPLACED_REMAINS_SHEET_ID = ""  // ID листа "Остатки не распределенные"
+const SEARCH_SHEET_ID = "" // ID листа "Поисковик"
+const CART_SHEET_ID = ""  // ID листа "Корзина"
+const EXCEL_REGISTRY_SHEET_ID = ""    // ID листа "Реестр файлов"
+const HANDBOOK_SHEET_ID = ""  // ID листа "Справочник"
 
-const EXCEL_REMAINS_FOLDER_ID = "1w3OMRiMvgo7FSu8qhx44MuMV823ab7Gc" // ID папки для выгрузки остатков
+const EXCEL_REMAINS_FOLDER_ID = "" // ID папки для выгрузки остатков
