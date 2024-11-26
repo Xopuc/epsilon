6b8e1ee48429ac0291ed7a8938e1c385699b55a3
Никита Кирьян <145542086+Xopuc@users.noreply.github.com>
2024-11-26 12:15:07 +0300

    Update _env.js

--- src/managerTable/_env.js
+++ src/managerTable/_env.js
@@ -1,10 +1,10 @@
-const MAIN_SS_ID = "1dnaBxu4z4cxp2Qv3lGyFpKa4BnjIGMoqP7KStG65a5k"   // ID Таблицы менеджера
-const REMAINS_TECH_SS_ID = "1FPP56421FdhErAc6DxoDV3o0NWRNDIWL2KimJzXPKUU"   // ID Таблицы для выгрузки менеджер
+const MAIN_SS_ID = ""   // ID Таблицы менеджера
+const REMAINS_TECH_SS_ID = ""   // ID Таблицы для выгрузки менеджер
 
-const REMAINS_SHEET_ID = "562607511"    // ID листа "Остатки"
-const REMAINS_TECH_SHEET_ID = "793192124"    // ID листа "Остатки (для выгрузки)"
-const SEARCH_SHEET_ID = "178869476" // ID листа "Поисковик"
-const EXCEL_REGISTRY_SHEET_ID = "1314426788"    // ID листа "Реестр файлов"
-const HANDBOOK_SHEET_ID = "1075101794"  // ID листа "Справочник"
+const REMAINS_SHEET_ID = ""    // ID листа "Остатки"
+const REMAINS_TECH_SHEET_ID = ""    // ID листа "Остатки (для выгрузки)"
+const SEARCH_SHEET_ID = "" // ID листа "Поисковик"
+const EXCEL_REGISTRY_SHEET_ID = ""    // ID листа "Реестр файлов"
+const HANDBOOK_SHEET_ID = ""  // ID листа "Справочник"
 
-const EXCEL_REMAINS_FOLDER_ID = "1w3OMRiMvgo7FSu8qhx44MuMV823ab7Gc" // ID папки для выгрузки остатков
+const EXCEL_REMAINS_FOLDER_ID = "" // ID папки для выгрузки остатков
